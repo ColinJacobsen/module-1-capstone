b24fd5cbf011b0f681c90c7eae2d8de3d9bb4066
Kenny <KennyCodes22@gmail.com>
2022-12-13 13:55:37 -0800

    Bookmark 1 Capstone

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,12 +1,34 @@
 package com.techelevator;
 
 import com.techelevator.view.Menu;
+import com.techelevator.view.Product;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE};
+
+	private static final String CUSTOMER_OPTION_FEED_MONEY = "Feed Money?";
+
+	private static final String CUSTOMER_OPTION_SELECT_PRODUCT = "Select Product";
+
+	private static final String CUSTOMER_OPTION_END_TRANSACTION = "End Transaction";
+
+	private static final String[] CUSTOMER_OPTIONS = {CUSTOMER_OPTION_FEED_MONEY, CUSTOMER_OPTION_SELECT_PRODUCT, CUSTOMER_OPTION_END_TRANSACTION};
+
+	private static File inventoryFile = new File("vendingmachine.csv");
+
+	private double balance = 0.0;
+
+	NumberFormat dollarFormat = NumberFormat.getCurrencyInstance();
 
 	private Menu menu;
 
@@ -19,16 +41,47 @@ public class VendingMachineCLI {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
+				// display vending machine items // create new method to display items from map
+				Map<String, Product> productMap = new HashMap<>();
+				try (Scanner inventoryScanner = new Scanner(inventoryFile)) {
+					while (inventoryScanner.hasNextLine()) {
+						String currentLine = inventoryScanner.nextLine();
+						String[] splitString = currentLine.split("\\|");
+						productMap.put(splitString[0], new Product(splitString[0], splitString[1], Double.parseDouble(splitString[2]), splitString[3]));
+					}
+				} catch (FileNotFoundException e) {
+					throw new RuntimeException(e);
+				}
+				for (Map.Entry<String, Product> product : productMap.entrySet()) {
+					System.out.println(product.getValue().getProductName());
+				}
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+
+				//BOOKMARK **********************************************************
+
+				while (true) {
+					System.out.println("Current Money Provided: " + dollarFormat.format(balance));
+					String customerChoice = (String) menu.getChoiceFromOptions(CUSTOMER_OPTIONS);
+					if (customerChoice.equals(CUSTOMER_OPTION_FEED_MONEY)) {
+						System.out.println("How many whole dollars did you deposit?");
+						try (Scanner input = new Scanner(System.in)) {
+							double depositAmount = input.nextDouble();
+							balance += depositAmount;
+						} catch (RuntimeException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}
 			}
 		}
 	}
+		public static void main (String[]args){
+			Menu menu = new Menu(System.in, System.out);
+			VendingMachineCLI cli = new VendingMachineCLI(menu);
+			cli.run();
 
-	public static void main(String[] args) {
-		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
-		cli.run();
+		}
+
 	}
-}
